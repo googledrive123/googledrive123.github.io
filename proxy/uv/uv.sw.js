24b8b158625f42a542656f8459e4bfe599e5311e
googledrive123 <altdiscsub@gmail.com>
2026-05-15 08:53:33 -0500

    remove proxy

--- proxy/uv/uv.sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('/proxy/uv/uv.bundle.js');
-importScripts('/proxy/uv/uv.config.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) =>
-  event.respondWith(sw.fetch(event))
-);
